19df3d84a60970c4e7bb6d093a86a0597b331730
SP7TECH <spandey247789@gmail.com>
2026-08-04 02:53:28 +0530

    component reusability & props in pizza component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-const Pizza = () => {
+const Pizza = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Pizza Name"),
-    React.createElement("h1", {}, "Pizza Description, blah, blah, blah"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
   ]);
 };
 

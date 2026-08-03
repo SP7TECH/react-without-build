a4027730b2527f72c1e0ebab3ae4a7322b8cfdfb
SP7TECH <spandey247789@gmail.com>
2026-08-04 02:51:32 +0530

    Pizza component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+const Pizza = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pizza Name"),
+    React.createElement("h1", {}, "Pizza Description, blah, blah, blah"),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
-  );
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+  ]);
 };
 
 const container = document.getElementById("root");

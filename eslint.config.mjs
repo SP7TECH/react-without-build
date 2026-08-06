2c3f0ba2025adf9c85713f425bf2365932dc7510
SP7TECH <spandey247789@gmail.com>
2026-08-07 04:23:31 +0530

    added eslint config for react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -15,7 +22,11 @@ export default [
         },
       },
     },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+    },
   },
-
+  reactPlugin.configs.flat["jsx-runtime"],
   prettier,
 ];

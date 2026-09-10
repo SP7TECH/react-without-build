1cc13fd639a18e7b934bb07ca5fa2929d25ff83d
SP7TECH <spandey247789@gmail.com>
2026-09-10 13:57:32 +0530

    using modal in past order component

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -2,17 +2,31 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import getPastOrders from "../api/getPastOrders";
+import getPastOrder from "../api/getPastOrders";
+import Modal from "../Modal";
+import { intl } from "../utils";
 
 export const Route = createLazyFileRoute("/past")({
   component: PastOrdersRoute,
 });
 
 function PastOrdersRoute() {
+  const [focusedOrder, setFocusedOrder] = useState;
   const [page, setPage] = useState(1);
+
+  // Query for all past orders
   const { isLoading, data } = useQuery({
     queryKey: ["past-orders", page],
     queryFn: () => getPastOrders(page),
     staleTime: 30000,
+  });
+
+  // Query for a specific order
+  const { isLoading: isLoadingPastOrder, data: pastOrderData } = useQuery({
+    queryKey: ["past-order", focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder),
+    enabled: !!focusedOrder,
+    staleTime: 24 * 60 * 60 * 1000, // one day in milliseconds,
   });
 
   if (isLoading) {
@@ -52,6 +66,46 @@ function PastOrdersRoute() {
           Next
         </button>
       </div>
+
+      {/* Focused Order Modal  */}
+      {focusedOrder ? (
+        <Modal>
+          <h2>Order #{focusedOrder}</h2>
+
+          {!isLoadingPastOrder ? (
+            <table>
+              <thead>
+                <tr>
+                  <td>Image</td>
+                  <td>Name</td>
+                  <td>Size</td>
+                  <td>Quantity</td>
+                  <td>Price</td>
+                  <td>Total</td>
+                </tr>
+              </thead>
+              <tbody>
+                {pastOrderData.orderItems.map((pizza) => (
+                  <tr key={`${pizza.pizzaTypeId}_${pizza.size}`}>
+                    <td>
+                      <img src={pizza.image} alt={pizza.name} />
+                    </td>
+                    <td>{pizza.name}</td>
+                    <td>{pizza.size}</td>
+                    <td>{pizza.quantity}</td>
+                    <td>{intl.format(pizza.price)}</td>
+                    <td>{intl.format(pizza.total)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>Loading...</p>
+          )}
+
+          <button onClick={() => setFocusedOrder()}>Close</button>
+        </Modal>
+      ) : null}
     </div>
   );
 }
